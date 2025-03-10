8f015111b0d8e9c31d6aed905d9865bc5a25ae15
arieldelg <arieldelgrande@hotmail.com>
2025-03-10 15:39:31 -0700

    refactor: adding conditianl of dev mode or prod mode

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,19 @@
 import * as admin from "firebase-admin";
 
-admin.initializeApp({
-  credential: admin.credential.cert("./lamudaCredentials.json"),
-});
+if (process.env.STAGE === "prod") {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      clientEmail: process.env.CLIENT_EMAIL,
+      privateKey: process.env.PRIVATE_KEY,
+      projectId: process.env.PROJECT_ID,
+    }),
+    projectId: process.env.PROJECT_ID,
+  });
+} else {
+  admin.initializeApp({
+    credential: admin.credential.cert("./lamudaCredentials.json"),
+  });
+}
 
 const db = admin.firestore();
 
